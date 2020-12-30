5d6473bc3d84e4a6c17020be498218d8954281de
Adriano Dias <adriano.rj.br@gmail.com>
2020-12-30 15:58:44 -0300

    index page first version completed

--- indexl.js
+++ indexl.js
@@ -1,3 +1,3 @@
-const selection = document.querySelector("#main");
-selection.textContent = "teste de dado induzido";
-setInterval(() => selection.classList.toggle("toggle"), 500);
+// const selection = document.querySelector("#main");
+// selection.textContent = "teste de dado induzido";
+// setInterval(() => selection.classList.toggle("toggle"), 500);
