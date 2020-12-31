ca97215afaea4fe10731fd5317be8cfb288c27ae
Adriano Dias <adriano.rj.br@gmail.com>
2020-12-31 13:30:30 -0300

    javascript effects and new version of index (Home) page.

--- indexl.js
+++ indexl.js
@@ -1,3 +1,10 @@
-// const selection = document.querySelector("#main");
-// selection.textContent = "teste de dado induzido";
-// setInterval(() => selection.classList.toggle("toggle"), 500);
+const selections = document.querySelectorAll(".picture");
+selections.forEach(function (selection) {
+  setInterval(() => selection.classList.toggle("toggle_shadow"), 3000);
+});
+const switches = document.querySelectorAll(".switch_pic");
+switches.forEach(function (switched) {
+  setInterval(() => {
+    switched.classList.toggle("off");
+  }, 6000);
+});
